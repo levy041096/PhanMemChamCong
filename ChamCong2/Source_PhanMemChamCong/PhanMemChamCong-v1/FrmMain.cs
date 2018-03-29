bb9b9cba00b73f31abc7debbef302abc859585b7
levy041096 <Letranthaovy041096@gmail.com>
2018-03-29 12:03:42 +0700

    add new source

---
+++ ChamCong2/Source_PhanMemChamCong/PhanMemChamCong-v1/FrmMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhanMemChamCong
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            FrmMain log = new FrmMain();
+            this.Close();
+            log.Show();
+        }
+
+        private void btnDangKy_Click(object sender, EventArgs e)
+        {
+            FrmDangKy dangky = new FrmDangKy();
+            this.Hide();
+            dangky.ShowDialog();
+            this.Close();
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            FrmDangNhap dangnhap = new FrmDangNhap();
+            this.Hide();
+            dangnhap.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ ChamCong2/Source_PhanMemChamCong/PhanMemChamCong-v1/resource/DangKy.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PhanMemChamCong.resource
+{
+    public class DangKy
+    {
+        private Dangnhap database;
+        public bool error;
+        private string m_hoten;
+        private string m_MaNVFT;
+        private string m_username;
+        private string m_password;
+        private string m_XNpass;
+        public string EmpError;
+
+        public string DK_hotenNV
+        {
+            get { return m_hoten; }
+            set { m_hoten = value; }
+        }
+        public string DK_maNVFT
+        {
+            get { return m_MaNVFT; }
+            set { m_MaNVFT = value; }
+        }
+        public string DK_user 
+        {
+            get { return m_username; }
+            set { m_username = value; }
+        }
+        public string DK_pass
+        {
+            get { return m_password; }
+            set { m_password = value; }
+        }
+        public string DK_xnpass
+        {
+            get { return m_XNpass; }
+            set { m_XNpass = value; }
+        }
+         public DangKy() { }
+         public bool saveDangky()
+         {
+             bool kayitDurumu;
+             bool ktManv;
+             bool ktten_user;
+             database = Dangnhap.getUser();
+             try
+             {
+                 SqlCommand cmd = new SqlCommand();
+                 cmd.CommandText = "Select Username From Dangky Where Username = @User";
+                 cmd.Connection = database.DatabaseConnectionOpen();
+                 cmd.Parameters.AddWithValue("@User", m_username);
+                 using (SqlDataReader reader = cmd.ExecuteReader())
+                 {
+                     if (reader.Read())
+                     {
+                         kayitDurumu = false;
+                     }
+                     else
+                     {
+                         reader.Close();
+                         kayitDurumu = true; cmd.CommandText = "Insert Into tbChamCong_DangNhap ( MaNVFT, User, Password) values (@MaNVFT, @User, @Password)";
+                        if ("@MaNVFT" != m_MaNVFT)
+                        {
+                            ktManv = false;
+                        }
+                         else
+                         {
+                             if ("@User" != "")
+                             {
+                                 ktten_user = false;
+                             }
+                             else
+                             {
+                                 cmd.Parameters.AddWithValue("@User", m_username);
+                                 cmd.Parameters.AddWithValue("@Password", m_password);
+                                 
+                             }
+                         }
+                          cmd.ExecuteNonQuery();                        
+                     }
+                 }
+             }
+             catch (Exception f)
+             {
+                 kayitDurumu = false;
+                 EmpError = f.ToString();
+             }
+             return kayitDurumu;
+         }
+    }
+}
